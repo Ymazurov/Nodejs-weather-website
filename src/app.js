3ef08d263d14af3e3f45dbff11285b3fdd8574f9
Ymazurov <ymazurov@tuta@io>
2020-03-04 03:40:06 +0200

    last attempt to push to heroku 2 electric boogaloo ver 3

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 const request = require('request')
-const geocode = require('./utils/geocode.js')
-const forecast = require('./utils/forecast.js')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 const city = process.argv[2]
 
 // Paths
@@ -96,6 +97,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`);
+})
